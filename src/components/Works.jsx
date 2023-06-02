fe124bc17fb6e2ae792c8340b10123e90a164211
TeddyFirman <teddyfirman902@gmail.com>
2023-06-03 00:12:58 +0700

    init works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -259,18 +259,18 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My portfolio</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
-      </motion.div>
+      {/* <motion.div variants={textVariant()}> */}
+      <p className={`${styles.sectionSubText} `}>My portfolio</p>
+      <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
+      {/* </motion.div> */}
 
       <div className='w-full flex'>
-        <motion.p
+        {/* <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-        >
-          Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described which is stored on my github. It reflects my ability to solve complex problems, work with different technologies and manage projects effectively.
-        </motion.p>
+        > */}
+        Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described which is stored on my github. It reflects my ability to solve complex problems, work with different technologies and manage projects effectively.
+        {/* </motion.p> */}
       </div>
 
       <div className='mt-20 flex flex-wrap gap-7'>
