9fb9f4024db0ee5be32c1d68864e3a459431a470
TeddyFirman <teddyfirman902@gmail.com>
2023-06-02 13:14:44 +0700

    init computers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { EarthCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto xs:bottom-12">
+    <div className="relative w-full h-screen mx-auto xs:bottom-12">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-4">
           <div className="w-5 h-5 rounded-full bg-gradient" />
@@ -21,12 +21,10 @@ const Hero = () => {
         </div>
       </div>
 
-      <div>
         <motion.div>
 
           <ComputersCanvas />
         </motion.div>
-      </div>
       {/* <EarthCanvas /> */}
 
 
@@ -47,7 +45,7 @@ const Hero = () => {
           </div>
         </a>
       </div>
-    </section>
+    </div>
   )
 }
 
