ea730f61356193feb3e39678a90e1e449eac920a
TeddyFirman <teddyfirman902@gmail.com>
2023-06-02 21:09:37 +0700

    init works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -92,100 +92,6 @@
 
 // INI BISA DIBUKA NG HPKU
 
-// import React from 'react';
-// import { Tilt } from "react-tilt";
-// import { motion } from 'framer-motion';
-
-// import { styles } from '../styles';
-// import { github } from '../assets';
-// import { SectionWrapper } from '../hoc';
-// import { projects } from '../constants';
-// import { fadeIn, textVariant } from '../utils/motion';
-
-// const ProjectCard = React.memo(({ index, name, description, tags, image, source_code_link }) => {
-//   return (
-//     <motion.div variants={fadeIn("up", "spring")}>
-//       <Tilt
-//         options={{
-//           max: 45,
-//           scale: 1,
-//           speed: 450
-//         }}
-//         className="bg-tertiary p-5 rounded-2xl w-full sm:w-[359px]"
-//       >
-//         <div className="relative w-full h-1/2 sm:h-[230px]">
-//           <img 
-//             src={image}
-//             alt={name}
-//             className="w-full h-full object-cover rounded-2xl"
-//           />
-
-//           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-//             <div
-//               onClick={() => window.open(source_code_link, "_blank")}
-//               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-//             >
-//               <img 
-//                 src={github}
-//                 alt="github"
-//                 className="w-1/2 h-1/2 object-contain"
-//               />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="mt-5">
-//           <h3 className="text-white font-bold text-xl sm:text-[24px]">{name}</h3>
-//           <p className="mt-2 text-secondary text-sm sm:text-[14px]">{description}</p>
-//         </div>
-
-//         <div className="mt-4 flex flex-wrap gap-2">
-//           {tags.map((tag) => (
-//             <p key={tag.name} className={`text-sm sm:text-[14px] ${tag.color}`}>
-//               #{tag.name}
-//             </p>
-//           ))}
-//         </div>
-//       </Tilt>
-//     </motion.div>
-//   )
-// })
-
-// const Works = () => {
-//   return (
-//     <>
-//       <motion.div variants={textVariant()}>
-//         <p className={styles.sectionSubText}>My portfolio</p>
-//         <h2 className={styles.sectionHeadText}>Projects</h2>
-//       </motion.div>
-
-//       <div className="w-full flex">
-//         <motion.p
-//           variants={fadeIn("", "", 0.1, 1)}
-//           className="mt-3 text-secondary text-base sm:text-[17px] max-w-3xl leading-[30px]"
-//         >
-//           Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described which is stored on my github. It reflects my ability to solve complex problems, work with different technologies and manage projects effectively.
-//         </motion.p>
-//       </div>
-
-//       <div className="mt-10 sm:mt-20 flex flex-wrap gap-7">
-//         {projects.map((project, index) => (
-//           <ProjectCard
-//             key={`project-${index}`}
-//             index={index}
-//             {...project}
-//           />
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default SectionWrapper(Works, "");
-
-
-// VERSI NYAR!!!!!!!!!!!!!!!!!!!!!!
-
 import React from 'react';
 import { Tilt } from "react-tilt";
 import { motion } from 'framer-motion';
@@ -205,7 +111,7 @@ const ProjectCard = React.memo(({ index, name, description, tags, image, source_
           scale: 1,
           speed: 450
         }}
-        className="bg-tertiary p-5 rounded-2xl w-full sm:w-[359px]"
+        className="bg-tertiary p-5 rounded-2xl w-full sm:w-[360px]"
       >
         <div className="relative w-full h-1/2 sm:h-[230px]">
           <img 
@@ -275,15 +181,8 @@ const Works = () => {
   )
 }
 
-const WrappedWorks = SectionWrapper(Works, "");
+export default SectionWrapper(Works, "");
 
-export default function App() {
-  return (
-    <>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <WrappedWorks />
-    </>
-  );
-}
+
+// VERSI NYAR!!!!!!!!!!!!!!!!!!!!!!
+
