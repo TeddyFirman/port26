1857a3552ed60f5b8a5a8fcc8afe07aafc6aec80
TeddyFirman <teddyfirman902@gmail.com>
2023-06-02 22:21:44 +0700

    init works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,7 +19,7 @@ const ProjectCard = React.memo(({ index, name, description, tags, image, source_
         }}
         className="bg-tertiary p-5 rounded-2xl w-full sm:w-[359px]"
       >
-        <div className="relative w-full h-1/3 sm:h-[230px]">
+        <div className="relative w-full h-1/3 sm:h-[230px] gap-7">
           <img 
             src={image}
             alt={name}
